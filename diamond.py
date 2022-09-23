a9174b5ecd791a1557f8c356fe1f63391be4766c
hemavarshiniloya <112843810+hemavarshiniloya@users.noreply.github.com>
2022-09-23 16:33:07 +0530

    Update diamond.py

--- diamond.py
+++ diamond.py
@@ -1,7 +1,7 @@
 import streamlit as st
-import pickel
-pickel_in=open('diamond.pkl','rb')
-mond=pickel.load(pickel_in)
+import pickle
+pickle_in=open('diamond.pkl','rb')
+mond=pickle.load(pickle_in)
 a=st.number_input('carat')
 b=st.number_input('cut')
 c=st.number_input('color')
